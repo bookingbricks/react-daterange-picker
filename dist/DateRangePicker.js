9b12597e5fa50c98a9b571b9fa356586765f687a
jontelm <jontelm@hotmail.com>
2017-01-13 15:24:07 +0100

    Remove console.log

--- dist/DateRangePicker.js
+++ dist/DateRangePicker.js
@@ -579,7 +579,6 @@ var DateRangePicker = _react2.default.createClass({
 
 
   render: function render() {
-    console.log(this.state, this.props);
     var _props3 = this.props,
         PaginationArrowComponent = _props3.paginationArrowComponent,
         className = _props3.className,
